def463150ea3a149ece60efee2f75a70ba051214
bangnd <bangnd@intra.nttdata.com.vn>
2018-11-19 20:54:28 +0700

    1. Config babel 2. Config webpack to start server and initial react

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "production",
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html?$/,
+        use: [
+					{
+						loader: 'html-loader',
+					},
+        ]
+      }, {
+        test: /\.js?$/,
+        exclude: [/node_modules/],
+        use: {
+					loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "./index.html"
+    })
+  ]
 };
